13eab908f1627215b6a3a258cb814431947e7cb0
Ana de Barros <scralowrealow@gmail.com>
2022-02-23 21:48:13 +0000

    Exercises 3.1 to 3.3

---
+++ exercises_3.1_to_3.3.py
@@ -0,0 +1,13 @@
+#3.1
+friends = ["daniela", "tostas", "isaura", "catia", "joão"]
+
+print(friends[0].title())
+print(friends[1].title())
+print(friends[2].title())
+
+#3.2
+print(f"Hey there {friends[1].title()} how are you?")
+
+#3.3
+motorbikes = ["Honda", "Kawasaki", "Husqvarna"]
+print(f"I would like to own a {motorbikes[2]} and ride, ride, ride!")
