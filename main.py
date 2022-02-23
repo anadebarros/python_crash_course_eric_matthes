9e1f1d91384f6be3a5951bd171fda22bf8166c87
Ana de Barros <scralowrealow@gmail.com>
2022-02-23 20:05:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+#2.3
+name = "marina"
+print(f"hey {name}, how are you?")
+#2.4
+print(name.lower())
+print(name.upper())
+print(name.title())
+#2.5
+author = "José Gomes Ferreira"
+print(f'{author} once said, \'E ninguém repara que não trago os olhos de ontem\'')
+#2.6
+quote = f"{author} once said, 'E ninguém repara que não trago os olhos de ontem'"
+print(quote)
+#2.7
+name2 = "   Ana   "
+print(f"my name is \n {name2}")
+print(len(name2))
+
+name3 = name2.lstrip()
+print(f"my name is \t {name3}")
+print(len(name3))
