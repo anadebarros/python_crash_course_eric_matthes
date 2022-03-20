f4b8eddeddbf169680fd3f9e56ae1dc06a5a4af9
Ana de Barros <scralowrealow@gmail.com>
2022-03-20 19:30:33 +0000

    added exercises 10.3 to 10.5

--- exercises_10.1_to_10.2.py
+++ exercises_10.1_to_10.2.py
@@ -42,3 +42,41 @@ try:
 except FileNotFoundError as err:
     print("This file was not found")
     raise err
+
+# 10.3
+name = input("Please tell me your name: ")
+
+try:
+    with open("guest.txt", mode="w") as my_file:
+        my_file.write(name)
+except FileNotFoundError as err:
+    print("Your file could not be found")
+    raise err
+
+# 10.4
+print("Enter 'exit' when you are finished.")
+while True:
+    name2 = input("Please tell me your name: ")
+    if name2 == "exit".lower():
+        break
+    else:
+        with open("guest_book.txt", mode="a") as my_file:
+            my_file.write(name2 + "\n")
+        print(f"Welcome {name2}!")
+
+# 10.5
+
+responses = []
+
+while True:
+    answer = input("Why do you like programming? ")
+    responses.append(answer)
+
+    quit = input("Would you like to quit? Y/N ")
+    if quit == "Y".lower():
+        break
+
+
+with open("poll.txt", mode="a") as my_file:
+    for response in responses:
+        my_file.write(response + "\n")
